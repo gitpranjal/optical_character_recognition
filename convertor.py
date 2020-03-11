838ef9415b6f299e7d04da2d3225f4e4e0fca774
pranjal <pranjal_bt2k15@dtu.ac.in>
2020-03-11 18:21:17 +0530

    converting pdf to text

---
+++ convertor.py
@@ -0,0 +1,80 @@
+from PIL import Image
+from pdf2image import convert_from_path
+
+# column = Image.open('code.png')
+# gray = column.convert('L')
+# blackwhite = gray.point(lambda x: 0 if x < 200 else 255, '1')
+# blackwhite.save("code_bw.jpg")
+
+# Path of the pdf
+PDF_file = "doc.pdf"
+# Store all the pages of the PDF in a variable
+pages = convert_from_path(PDF_file, 500)
+
+# Counter to store images of each page of PDF to image
+image_counter = 1
+
+# Iterate through all the pages stored above
+for page in pages:
+    # Declaring filename for each page of PDF as JPG
+    # For each page, filename will be:
+    # PDF page 1 -> page_1.jpg
+    # PDF page 2 -> page_2.jpg
+    # PDF page 3 -> page_3.jpg
+    # ....
+    # PDF page n -> page_n.jpg
+    filename = "page_" + str(image_counter) + ".jpg"
+
+    # Save the image of the page in system
+    page.save(filename, 'JPEG')
+
+    # Increment the counter to update filename
+    image_counter = image_counter + 1
+
+# print(tesserocr.tesseract_version())
+
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# custom_oem_psm_config = r'--oem 3 --psm 6'
+custom_oem_psm_config = r'--psm 6'
+
+# Variable to get count of total number of pages
+filelimit = image_counter - 1
+
+# Creating a text file to write the output
+outfile = "out_text.txt"
+
+# Open the file in append mode so that
+# All contents of all images are added to the same file
+f = open(outfile, "a")
+
+# Iterate from 1 to total number of pages
+for i in range(1, filelimit + 1):
+    # Set filename to recognize text from
+    # Again, these files will be:
+    # page_1.jpg
+    # page_2.jpg
+    # ....
+    # page_n.jpg
+    filename = "page_" + str(i) + ".jpg"
+
+    # Recognize the text as string in image using pytesserct
+    text = str(((pytesseract.image_to_string(Image.open(filename), config=custom_oem_psm_config))))
+    print(text)
+
+    # The recognized text is stored in variable text
+    # Any string processing may be applied on text
+    # Here, basic formatting has been done:
+    # In many PDFs, at line ending, if a word can't
+    # be written fully, a 'hyphen' is added.
+    # The rest of the word is written in the next line
+    # Eg: This is a sample text this word here GeeksF-
+    # orGeeks is half on first line, remaining on next.
+    # To remove this, we replace every '-\n' to ''.
+    text = text.replace('-\n', '')
+
+    # Finally, write the processed text to the file.
+    f.write(text)
+
+# Close the file after writing all the text.
+f.close()
